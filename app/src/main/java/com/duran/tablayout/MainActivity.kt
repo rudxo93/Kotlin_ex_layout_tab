50e28a26530cd298edfb3e5ad8ba628af58563a0
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-06-25 15:00:25 +0900

    MainActivity에 tabLayout과 viewPager 연동하여 구현

--- app/src/main/java/com/duran/tablayout/MainActivity.kt
+++ app/src/main/java/com/duran/tablayout/MainActivity.kt
@@ -2,16 +2,32 @@ package com.duran.tablayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.duran.tablayout.adapter.ViewPagerAdapter
 import com.duran.tablayout.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val tabTitleArray = arrayOf(
+        "Tab1",
+        "Tab2",
+        "Tab3"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val viewPager = binding.viewPager
+        val tabLayout = binding.tabLayout
+
+        viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabTitleArray[position]
+        }.attach()
 
     }
 }
